28fada1573445a5968cbb39f644e71bf3add36ec
polykernel <81340136+polykernel@users.noreply.github.com>
2024-10-15 20:22:32 -0400

    refactor: move prettify html step out of site-builder
    - Remove the prettifyHtml compiler in Hakyll and add it
     as a post-processing step in the website derivation

--- website.nix
+++ website.nix
@@ -3,6 +3,7 @@
   stdenv,
   site-builder,
   js-beautify,
+  glibcLocalesUtf8,
 }:
 
 stdenv.mkDerivation {
@@ -33,9 +34,16 @@ stdenv.mkDerivation {
     js-beautify
   ];
 
+  LANG = "en_US.UTF-8";
+  LOCALE_ARCHIVE = lib.optionalString (stdenv.buildPlatform.libc == "glibc") "${lib.getLib glibcLocalesUtf8}/lib/locale/locale-archive";
+
   buildPhase = ''
     runHook preBuild
+    # Build the site
     ${lib.getExe site-builder} build
+    # Format html files
+    echo 'Post-processing'
+    find _site -type f -name '*.html' -exec '${lib.getExe' js-beautify "js-beautify"}' -f '{}' -r --type html --no-preserve-newlines \;
     runHook postBuild
   '';
 
