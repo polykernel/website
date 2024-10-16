8d75a541f4a56f3e859b17d989076b0fbb69a2aa
polykernel <81340136+polykernel@users.noreply.github.com>
2024-10-15 22:59:08 -0400

    feat: add favicon
    - Add basic icon.svg as source of truth for all icon variants
    - Generate standard .ico files in 32x32 and 16x16 size
    as well as .png file for apple devices in the website
    derivation
    - Update default.html template to include website favicon

--- website.nix
+++ website.nix
@@ -4,6 +4,8 @@
   site-builder,
   js-beautify,
   glibcLocalesUtf8,
+  librsvg,
+  imagemagick,
 }:
 
 stdenv.mkDerivation {
@@ -20,6 +22,7 @@ stdenv.mkDerivation {
       ./CNAME
       ./404.html
       ./index.html
+      ./icon.svg
     ];
   };
 
@@ -32,6 +35,8 @@ stdenv.mkDerivation {
   nativeBuildInputs = [
     site-builder
     js-beautify
+    librsvg
+    imagemagick
   ];
 
   LANG = "en_US.UTF-8";
@@ -39,18 +44,30 @@ stdenv.mkDerivation {
 
   buildPhase = ''
     runHook preBuild
+
     # Build the site
     ${lib.getExe site-builder} build
+
     # Format html files
     echo 'Post-processing'
     find _site -type f -name '*.html' -exec '${lib.getExe' js-beautify "js-beautify"}' -f '{}' -r --type html --no-preserve-newlines \;
+
+    # Create favicons
+    rsvg-convert -h 16 -o icon-16.png icon.svg
+    rsvg-convert -h 32 -o icon-32.png icon.svg
+    magick icon-16.png icon-32.png _site/favicon.ico
+    rsvg-convert -h 192 -o icon-192.png icon.svg
+    magick icon-192.png -resize 140x140 -gravity center -background transparent -extent 180x180 -quality 100 _site/apple-touch-icon.png
+
     runHook postBuild
   '';
 
   installPhase = ''
     runHook preInstall
+
     mkdir $out
     cp -r _site/* $out
+
     runHook postInstall
   '';
 }
